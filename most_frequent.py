a38a6302963d59482176847b336b78596eb655c3
muskanali-2001 <79519471+muskanali-2001@users.noreply.github.com>
2021-03-28 22:09:21 +0530

    create most_ferquency.py

---
+++ most_frequent.py
@@ -0,0 +1,20 @@
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+dict1=most_frequent('Mississippi')
+sorted_values = sorted(dict1.values())
+sorted_dict = {}
+
+for i in sorted_values:
+    for k in dict1.keys():
+        if dict1[k] == i:
+            sorted_dict[k] = dict1[k]
+            break
+
+print(sorted_dict)
